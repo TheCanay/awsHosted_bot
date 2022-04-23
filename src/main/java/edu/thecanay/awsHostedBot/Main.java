52965060c304fa02a12ea6b89b6b8c04ec727a59
TheCanay <liubomurpryimak@gmail.com>
2022-04-23 09:45:29 +0300

    base bot that send same message as yours implemented

---
+++ src/main/java/edu/thecanay/awsHostedBot/Main.java
@@ -0,0 +1,17 @@
+package edu.thecanay.awsHostedBot;
+
+import edu.thecanay.awsHostedBot.bot.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException exception) {
+
+        }
+    }
+}
